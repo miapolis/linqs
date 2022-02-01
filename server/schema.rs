d735c28b8f30d4c611d98d3f93690b75c45717e7
Ethan Lerner <41233357+Miapolis@users.noreply.github.com>
2022-01-31 23:15:51 -0500

    update diesel schemas

--- server/schema.rs
+++ server/schema.rs
@@ -1,4 +1,12 @@
 table! {
+    auth_infos (id) {
+        id -> Int4,
+        user_id -> Int4,
+        password_hash -> Text,
+    }
+}
+
+table! {
     link_items (id) {
         id -> Varchar,
         url -> Varchar,
@@ -16,6 +24,28 @@ table! {
     }
 }
 
+table! {
+    posts (id) {
+        id -> Int4,
+        title -> Varchar,
+        body -> Text,
+        published -> Bool,
+    }
+}
+
+table! {
+    users (id) {
+        id -> Int4,
+        username -> Varchar,
+    }
+}
+
 joinable!(link_uses -> link_items (link_item_id));
 
-allow_tables_to_appear_in_same_query!(link_items, link_uses,);
+allow_tables_to_appear_in_same_query!(
+    auth_infos,
+    link_items,
+    link_uses,
+    posts,
+    users,
+);
